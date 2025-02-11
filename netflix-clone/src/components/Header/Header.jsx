a7b5488f7ce444890f3a52b776b8d0ccab97c846
yidnek-sudo <53978015+yidnek-sudo@users.noreply.github.com>
2025-02-10 21:58:48 -0800

    Header and Footer

---
+++ netflix-clone/src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import"./Header.css"
+import NetflixLogo from "../../assets/images/neflixlogo.jpeg"
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownCircleIcon from "@mui/icons-material/ArrowDropDownCircle";
+
+const Header = () => {
+  return (
+    <div className="header_outer-container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li><img src={NetflixLogo} alt="Netflix Logo" width="50"/></li>
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>TvShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Language</li>
+          </ul>
+        </div>
+      </div>
+      <div className='header_right'>
+        <ul>
+            <li><SearchIcon/></li>
+            <li><NotificationsNoneIcon/></li>
+            <li><AccountBoxIcon/></li>
+            <li><ArrowDropDownCircleIcon/></li>
+            
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header
---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/> 
+        <br />
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
